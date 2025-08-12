a2fcf95a4203de9bf049ee1f7c3a76fcbb6dd9eb
emreyavuz <emreyavuz89@hotmail.com>
2025-08-12 14:31:20 +0300

    init

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "GlassCard",
+            name: "GlassCardSDK",
             targets: ["GlassCard"]),
     ],
     targets: [
