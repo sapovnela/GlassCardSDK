08ad55b20f9ea5de1611f6d7eb965d35413dfacf
emreyavuz <emreyavuz89@hotmail.com>
2025-08-12 14:28:30 +0300

    init

--- Package.swift
+++ Package.swift
@@ -8,14 +8,13 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "GlassCardSDK",
-            targets: ["GlassCardSDK"]),
+            name: "GlassCard",
+            targets: ["GlassCard"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "GlassCardSDK"),
+        .binaryTarget(name: "GlassCard", path: "./Sources/GlassCard.xcframework"),
 
     ]
 )
